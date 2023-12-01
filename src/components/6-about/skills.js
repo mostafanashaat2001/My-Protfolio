97ce65846ac66d6127b47fa9020061d3dcacf8f0
mostafa <mostafa.nashaat2001@gmail.com>
2023-12-01 02:05:33 +0200

    change the image path

--- src/components/6-about/skills.js
+++ src/components/6-about/skills.js
@@ -1,67 +1,67 @@
 export const Skills = [
   {
     id: 1,
-    image: "../../../public/images/icons/html-5.png",
+    image: "./images/icons/html-5.png",
     title: "HTML 5",
   },
   {
     id: 2,
-    image: "../../../public/images/icons/css.png",
+    image: "./images/icons/css.png",
     title: "CSS 3",
   },
   {
     id: 3,
-    image: "../../../public/images/icons/java-script.png",
+    image: "./images/icons/java-script.png",
     title: "JavaScript ES6",
   },
   {
     id: 4,
-    image: "../../../public/images/icons/React.png",
+    image: "./images/icons/React.png",
     title: "React JS",
   },
   {
     id: 5,
-    image: "../../../public/images/icons/Redux.png",
+    image: "./images/icons/Redux.png",
     title: "Redux",
   },
   {
     id: 6,
-    image: "../../../public/images/icons/next-js.svg",
+    image: "./images/icons/next-js.svg",
     title: "Next JS",
   },
   {
     id: 7,
-    image: "../../../public/images/icons/jest.png",
+    image: "./images/icons/jest.png",
     title: "Jest",
   },
   {
     id: 8,
-    image: "../../../public/images/icons/bootstrap.png",
+    image: "./images/icons/bootstrap.png",
     title: "Bootstrap 5",
   },
   {
     id: 9,
-    image: "../../../public/images/icons/sass.png",
+    image: "./images/icons/sass.png",
     title: "Sass",
   },
   {
     id: 10,
-    image: "../../../public/images/icons/node.png",
+    image: "./images/icons/node.png",
     title: "Node JS",
   },
   {
     id: 11,
-    image: "../../../public/images/icons/express.png",
+    image: "./images/icons/express.png",
     title: "Express Js",
   },
   {
     id: 12,
-    image: "../../../public/images/icons/mongodb.svg",
+    image: "./images/icons/mongodb.svg",
     title: "MongoDB",
   },
   {
     id: 13,
-    image: "../../../public/images/icons/wordpress.png",
+    image: "./images/icons/wordpress.png",
     title: "WordPress",
   },
 ];
